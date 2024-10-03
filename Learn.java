f2e245be9f71181b656584970892a68a287e6c43
Pedro Madruga <pedromadruga6@gmail.com>
2024-10-03 10:26:08 -0300

    Add files via upload

---
+++ Learn.java
@@ -0,0 +1,64 @@
+// public class Learn {
+// 	public static void main(String[] args) {
+
+// 					System.out.println("Hello World");
+// 		// Em JS -> console.log("Hello world")
+// 	}
+// }
+
+public class Learn {
+	public static void main(String[] args) {
+
+		// VARIABLES
+		String surname = "Madruga";
+		int myAge = 22;
+		myAge = 26;
+		System.out.println("Pedro " + surname + " have " + myAge + " years.");
+
+		String car = "Lamborghini";
+		String model = " Aventador SVJ";
+		String carModel = car  +  model;
+
+		System.out.println("The BEST car is " + carModel);
+
+		int x = 10;
+		int y = 12;
+
+		System.out.println(x + y);
+
+		// DECLARE MANY VARIABLES
+		int p = 5, h = 10, z = 26;
+		System.out.println(p + h + z);
+
+		int a, b, c;
+		a = b = c = 100;
+		System.out.println(a + b + c);
+
+		// REAL EXAMPLE
+		String studentName = "Pedro Madruga";
+		int studentId = 10;
+		int studentAge = 22;
+		float studentFee = 75.30f;
+		char studentGrade = 'A';
+
+		System.out.println("Student name: " + studentName);
+		System.out.println("Student id: " + studentId);
+		System.out.println("Student age: " + studentAge);
+		System.out.println("Student fee: " + studentFee);
+		System.out.println("Student grade: " + studentGrade);
+
+		// CALCULE RECTANGLE AREA
+		int length = 10;
+		float width = 28.3f;
+		float area;
+
+		area = length * width;
+
+		System.out.println("Lenght: " + length);
+		System.out.println("Width: " + width);
+		System.out.println("Area: " + area + "m2");
+	}
+}
+
+
+		
